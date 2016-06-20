8b13ea07ee6fd39a73cd97f0630d254e7e567aae
5adiyah <sadiali@pdx.edu>
2016-06-20 09:45:52 -0700

    Add game model attributes

--- app/models/game.js
+++ app/models/game.js
@@ -1,7 +1,15 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
+import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-
+  name: attr(),
+  users: attr(),
+  organization: attr(),
+  theme: attr(),
+  rules: attr(),
+  weapon: attr(),
+  adminName: attr(),
+  adminPassword: attr(),
+  users: hasMany('user', {async: true})
 });
