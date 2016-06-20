2c09aaf7f1a8819c205ff56d515871a2002d2c8b
scott holland <scotthollandjr@gmail.com>
2016-06-20 13:36:19 -0700

    add user model and save user when sign up completed

--- app/components/sign-up-component.js
+++ app/components/sign-up-component.js
@@ -1,23 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  firebaseApp: Ember.inject.service(),
-
   actions: {
     createUser() {
-      var email = this.get('email');
-      var password = this.get('password');
-      const auth = this.get('firebaseApp').auth();
-      var self = this;
-      auth.createUserWithEmailAndPassword(email, password).then((userResponse) => {
-        debugger;
-        const user = self.store.createRecord('user', {
-          id: userResponse.uid,
-          email: userResponse.email
-        });
-        return user.save();
-      });
+      var params = {
+        email: this.get('email'),
+        password: this.get('password')
+      };
+      this.sendAction('createUser', params);
     }
-
   }
 });
--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -2,6 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    return Ember.RSVP.hash({
+      users: this.store.findAll('user')
+    });
+  },
+  firebaseApp: Ember.inject.service(),
+
+  actions: {
+    createUser(params) {
+      var email = params.email;
+      var password = params.password;
+      const auth = this.get('firebaseApp').auth();
+      var self = this;
+      auth.createUserWithEmailAndPassword(email, password).then((userResponse) => {
+        debugger;
+        const user = self.store.createRecord('user', {
+          id: userResponse.uid,
+          email: userResponse.email
+        });
+        return user.save();
+      });
+    }
 
   }
 });
