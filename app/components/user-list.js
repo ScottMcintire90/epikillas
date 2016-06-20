45446ba75a8b0d0341b7fe9c55ea60b2befb5a00
k <Guest@epicodus-55.local>
2016-06-20 16:25:06 -0700

    kinda almost there on populating a stupid list.

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     displayUser() {
-      debugger;
-      
-      console.log(this.get('games').get('firstObject').get('user'));
-
+      var userList = this.model.get('user');
+      var splitUsers = userList.split(",");
+      for (var i=0; i < splitUsers.length; ++i) {
+        console.log(splitUsers.pop());
+        return splitUsers.pop();
+      }
     }
   }
 });
